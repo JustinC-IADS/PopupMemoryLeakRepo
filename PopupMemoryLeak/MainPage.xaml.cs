2f401a187659d4e083fadbaf390bcc22fe7d35a2
Justin Cook <justin.w.cook.civ@mail.mil>
2022-12-28 09:07:44 -0600

    Updated to .net7 and newest version of toolkit

--- PopupMemoryLeak/MainPage.xaml.cs
+++ PopupMemoryLeak/MainPage.xaml.cs
@@ -11,19 +11,21 @@ public partial class MainPage : ContentPage
 
     private async void Button1_Clicked(object sender, EventArgs e)
     {
-        _ = new CustomPopup("Created, but not opened");
+        new CustomPopup("Created, but not opened");
     }
 
     private async void Button2_Clicked(object sender, EventArgs e)
     {
-        _ = await this.ShowPopupAsync(new CustomPopup("Opened from MainPage")).ConfigureAwait(true);
+        await this.ShowPopupAsync(new CustomPopup("Opened from MainPage")).ConfigureAwait(true);
     }
 
     private async void Button3_Clicked(object sender, EventArgs e)
     {
         ContentPage newPage = new ContentPage() { Title = "New Page" };
         await Navigation.PushAsync(newPage);
-        _ = await newPage.ShowPopupAsync(new CustomPopup("Opened using New Page")).ConfigureAwait(true);
+
+        await Task.Delay(500).ConfigureAwait(true);
+        await newPage.ShowPopupAsync(new CustomPopup("Opened using New Page")).ConfigureAwait(true);
     }
 
     private async void Button4_Clicked(object sender, EventArgs e)
